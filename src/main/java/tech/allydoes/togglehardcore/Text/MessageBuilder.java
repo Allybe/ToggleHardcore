3d0be59ae6294237dca5ed52c90ec7aa9a957e2c
Allison <JAMIRANDA4@CPS.EDU>
2024-06-23 00:37:16 -0500

    colors fixed and dupe bug fixed

--- src/main/java/tech/allydoes/togglehardcore/Text/MessageBuilder.java
+++ src/main/java/tech/allydoes/togglehardcore/Text/MessageBuilder.java
@@ -1,19 +1,21 @@
 package tech.allydoes.togglehardcore.Text;
 
+import org.bukkit.ChatColor;
+
 public class MessageBuilder {
     public static String getMessage(String message, MessageLevel level) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("ToggleHardcore|");
+        stringBuilder.append("ToggleHardcore | ");
 
         switch (level) {
             case ERROR:
-                stringBuilder.append("§c" + "ERROR: ");
+                stringBuilder.append(ChatColor.RED + "ERROR: ");
                 break;
             case WARNING:
-                stringBuilder.append("§6" + "WARNING: ");
+                stringBuilder.append(ChatColor.GOLD + "WARNING: ");
                 break;
             case INFO:
-                stringBuilder.append("§f" + "INFO: ");
+                stringBuilder.append(ChatColor.WHITE + "INFO: ");
                 break;
         }
 
